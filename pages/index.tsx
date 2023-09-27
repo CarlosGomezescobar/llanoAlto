f4a9cfd1bf81cd455c083e3436d1b352679d038c
iUranoO <uranoitech@gmail.com>
2023-09-27 00:12:00 -0400

    IA

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Layout from "../components/layout"
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>Llano Alto IA</h1>
       <p>
         This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
+        <a href="#">Llano Alto IA</a> por Authenticar.
       </p>
     </Layout>
   )
